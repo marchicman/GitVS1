cd06bcb7fad3c2aff85906f373e589d20350f713
marchicman <manlio.marchica@live.it>
2017-08-06 07:52:59 +0200

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //Changes done in GitHub
         }
     }
 }
